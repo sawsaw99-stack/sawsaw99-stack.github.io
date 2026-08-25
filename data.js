f81f4ccc8815e8466e6709b320c5bce1d3ecdfe6
Sawyer <brausaw30@gmail.com>
2026-08-25 14:38:13 -0500

    Add socialsData and projectsData arrays in data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+const socialsData = [
+  {
+    title: "GitHub",
+    handle: "@sawsaw99-stack",
+    url: "https://github.com/sawsaw99-stack",
+    icon: "fa-brands fa-github",
+    color: "indigo"
+  },
+  {
+    title: "YouTube",
+    handle: "@sawyer-does-stuff",
+    url: "https://youtube.com/@sawyer-does-stuff",
+    icon: "fa-brands fa-youtube",
+    color: "red"
+  },
+  {
+    title: "Suno",
+    handle: "@sawyerdoesstuffyt",
+    url: "https://suno.com/@sawyerdoesstuffyt",
+    icon: "fa-solid fa-music",
+    color: "amber"
+  }
+];
+
+const projectsData = [
+  {
+    title: "Star Rating Evaluator",
+    category: "Python / Tkinter",
+    description: "Evaluator app featuring nested factor trees and JSON I/O.",
+    tags: ["Python", "Tkinter", "JSON"],
+    codeUrl: "https://github.com/sawsaw99-stack"
+  }
+  // Add more project objects here
+];
